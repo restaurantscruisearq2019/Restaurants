97329b275ea1a90c2028ed86d42840c1863eb26c
miacastroco <miacastroco@unal.edu.co>
2019-06-14 23:16:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 require("./routes/index")(app);
 require("./routes/restaurants")(app);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 console.log("App listening at localhost:" + PORT);
